f67531cfeff8818d44ada7e46ac93b641bf900b6
alexeysp11 <alexeyspiridon1111@gmail.com>
2021-06-30 00:10:43 +0500

    Initial commit

---
+++ Source/Pages/Auth/Login.cshtml.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using BookList.Models;
+using BookList.Services; 
+
+namespace BookList.Pages
+{
+    public class LoginModel : PageModel 
+    {
+        public string Greeting { get; set; } = "Hello"; 
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
