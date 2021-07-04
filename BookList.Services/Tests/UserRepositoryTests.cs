059e91ce730b1b134e0a0f2e2241e72df1bcc7b3
alexeysp11 <alexeyspiridon1111@gmail.com>
2021-07-04 23:20:30 +0500

    Added tests for Services

--- BookList.Services/Tests/UserRepositoryTests.cs
+++ BookList.Services/Tests/UserRepositoryTests.cs
@@ -24,21 +24,14 @@ namespace Tests.BookList.Services
         private readonly string pathToDb = "C:\\Users\\User\\Desktop\\projects\\AspNetCore\\BookList\\Databases\\TestDB.sqlite3"; 
         #endregion  // Configurational settings
 
-        #region Constructors
-        /// <summary>
-        /// Default constructor for initializing DbHelper and _UserRepository. 
-        /// </summary>
-        public UserRepositoryTests()
-        {
-            DbHelper = new SqliteDbHelper(pathToDb); 
-            _UserRepository = new UserRepository(DbHelper);
-        }
-        #endregion  // Constructors
-
         #region Create 
         [Fact]
         public void Initialized_CheckIfDatabaseExists_DatabaseExists()
         {
+            // Arrange. 
+            DbHelper = new SqliteDbHelper(pathToDb); 
+            _UserRepository = new UserRepository(DbHelper);
+
             // Act. 
             bool exists = System.IO.File.Exists(pathToDb); 
 
@@ -50,6 +43,9 @@ namespace Tests.BookList.Services
         public void CreateUser_Invoke_UserExistsInDatabase()
         {
             //Given
+            DbHelper = new SqliteDbHelper(pathToDb); 
+            _UserRepository = new UserRepository(DbHelper);
+            
             string fullname = "Mark"; 
             string country = "USA"; 
             string city = "Portland"; 
